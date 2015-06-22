dfbc18bc7d76aa9bde5f0c0ee175f751f3e5ea0f
Nicolas Thal <nico.th4l@gmail.com>
2015-06-22 10:44:24 +0200

    complete locale listener test

--- BaseBundle/Tests/EventListener/LocaleListenerTest.php
+++ BaseBundle/Tests/EventListener/LocaleListenerTest.php
@@ -62,4 +62,50 @@ class LocaleListenerTest extends \PHPUnit_Framework_TestCase
         Phake::verify($this->request, Phake::never())->getSession();
         Phake::verify($this->request, Phake::never())->setLocale();
     }
+
+    /**
+     * @param string      $sessionLocale
+     * @param string|null $attributesLocale
+     *
+     * @dataProvider provideSessionAndAttributeLocale
+     */
+    public function testWithPreviousSession($sessionLocale, $attributesLocale = null)
+    {
+        $attributes = Phake::mock('Symfony\Component\HttpFoundation\ParameterBag');
+        Phake::when($attributes)->get(Phake::anyParameters())->thenReturn($attributesLocale);
+
+        $session = Phake::mock('Symfony\Component\HttpFoundation\Session\SessionInterface');
+        Phake::when($session)->get(Phake::anyParameters())->thenReturn($sessionLocale);
+
+        Phake::when($this->request)->hasPreviousSession()->thenReturn(true);
+        Phake::when($this->request)->getSession()->thenReturn($session);
+        $this->request->attributes = $attributes;
+
+        $this->listener->onKernelRequest($this->event);
+
+        if (is_null($attributesLocale)) {
+            Phake::verify($this->request)->setLocale($sessionLocale);
+            Phake::verify($session)->get('_locale', 'en');
+            Phake::verify($session, Phake::never())->set(Phake::anyParameters());
+        } else {
+            Phake::verify($session)->set('_locale', $attributesLocale);
+            Phake::verify($this->request, Phake::never())->setLocale(Phake::anyParameters());
+            Phake::verify($session, Phake::never())->get(Phake::anyParameters());
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function provideSessionAndAttributeLocale()
+    {
+        return array(
+            array('en'),
+            array('fr'),
+            array('en', 'en'),
+            array('fr', 'en'),
+            array('en', 'fr'),
+            array('fr', 'fr'),
+        );
+    }
 }
