67631bd4ff0b015353251c34955647843bf7955e
soleng <sola-prog@live.fr>
2015-11-06 17:34:36 +0100

    add version project

--- BaseBundle/OpenOrchestraBaseBundle.php
+++ BaseBundle/OpenOrchestraBaseBundle.php
@@ -11,6 +11,13 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class OpenOrchestraBaseBundle extends Bundle
 {
+    const VERSION = '1.0.1';
+    const VERSION_ID = 10001;
+    const MAJOR_VERSION = 1;
+    const MINOR_VERSION = 0;
+    const RELEASE_VERSION = 1;
+    const EXTRA_VERSION = 'STABLE';
+
     /**
      * @param ContainerBuilder $container
      */
