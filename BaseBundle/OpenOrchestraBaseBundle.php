e66adbecd92b8ca3d86a7aad8de111ebf7de13ed
Amaury Lavieille <amaury.lavieille@gmail.com>
2016-08-16 12:31:06 +0200

    update tag version 1.1.4 (#108)

--- BaseBundle/OpenOrchestraBaseBundle.php
+++ BaseBundle/OpenOrchestraBaseBundle.php
@@ -11,11 +11,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class OpenOrchestraBaseBundle extends Bundle
 {
-    const VERSION = '1.1.3';
+    const VERSION = '1.1.4';
     const VERSION_ID = 10103;
     const MAJOR_VERSION = 1;
     const MINOR_VERSION = 1;
-    const RELEASE_VERSION = 3;
+    const RELEASE_VERSION = 4;
     const EXTRA_VERSION = 'STABLE';
 
     /**
