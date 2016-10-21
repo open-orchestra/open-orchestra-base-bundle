b4f5c604fcf7c128b4c337f5445624fa2b71cb49
itkg-ngilain <noel.gilain@businessdecision.com>
2016-10-21 11:15:38 +0200

    Update version to 1.1.6-dev

--- BaseBundle/OpenOrchestraBaseBundle.php
+++ BaseBundle/OpenOrchestraBaseBundle.php
@@ -11,12 +11,12 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class OpenOrchestraBaseBundle extends Bundle
 {
-    const VERSION = '1.1.5';
-    const VERSION_ID = 10105;
+    const VERSION = '1.1.6-DEV';
+    const VERSION_ID = 10106;
     const MAJOR_VERSION = 1;
     const MINOR_VERSION = 1;
-    const RELEASE_VERSION = 5;
-    const EXTRA_VERSION = 'STABLE';
+    const RELEASE_VERSION = 6;
+    const EXTRA_VERSION = 'DEV';
 
     /**
      * @param ContainerBuilder $container
