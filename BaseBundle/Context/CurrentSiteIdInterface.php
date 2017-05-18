dd72cbe34dadf0c1b97d481e536e52904a8061fe
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2017-05-18 10:58:56 +0200

    refacto context manager

--- BaseBundle/Context/CurrentSiteIdInterface.php
+++ BaseBundle/Context/CurrentSiteIdInterface.php
@@ -2,22 +2,23 @@
 
 namespace OpenOrchestra\BaseBundle\Context;
 
-
 /**
  * Interface CurrentSiteIdInterface
+ *
+ * @deprecated use OpenOrchestra\DisplayBundle\Manager\ContextInterface or OpenOrchestra\BackOffice\Manager\ContextInterface
  */
 interface CurrentSiteIdInterface
 {
     /**
      * Get the current site id
-     *
+     * @deprecated
      * @return string
      */
     public function getCurrentSiteId();
 
     /**
      * Get the current default language of the current site
-     *
+     * @deprecated
      * @return string
      */
     public function getCurrentSiteDefaultLanguage();
